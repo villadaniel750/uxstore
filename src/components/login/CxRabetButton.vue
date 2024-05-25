1a5a7e2f861d8b367f4eceec07aeed72f761c394
villanuevaquelin <villanuevaquelin@gmail.com>
2024-05-25 11:58:11 -0300

    fix rabet button light mode

--- src/components/login/CxRabetButton.vue
+++ src/components/login/CxRabetButton.vue
@@ -1,11 +1,12 @@
 <template>
-    <v-btn @click="userSignTransaction" block  rel="noopener noreferrer"  variant="tonal" style="border-color: #B8BAC4; color: #B8BAC4;" class="text-capitalize mt-3" size="large">
+    <v-btn @click="userSignTransaction" block  rel="noopener noreferrer"  variant="tonal" :color="isDarkTheme ? '#B8BAC4' : 'grey-darken-1'"  class="text-capitalize mt-3" size="large">
         <img src="@/assets/rabet.svg" alt="Twitter" style="height: 24px; width: 24px; margin-right: 8px;"/>
         Rabet
     </v-btn>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
     methods: {
         async userSignTransaction() {
@@ -30,6 +31,11 @@ export default {
                 return error.toString();
             }
         }
-    }
+    },
+    computed: {
+      ...mapGetters([
+        'isDarkTheme'
+      ]),
+    },
 };
 </script>

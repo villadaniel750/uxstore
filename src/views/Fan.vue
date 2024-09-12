a498b3a6441f23f409d78d6fcb30e3847628e9a4
villanuevaquelin <villanuevaquelin@gmail.com>
2024-09-12 15:03:50 -0300

    tabs in fanpage

--- src/views/Fan.vue
+++ src/views/Fan.vue
@@ -1,6 +1,21 @@
 <template>
     <v-container >
         <cx-fan-header></cx-fan-header>
+        <v-card class="fill-height">
+    <v-tabs v-model="tab" color="orange-accent-2" align-tabs="left">
+      <v-tab value="deposit">Entries</v-tab>
+      <v-tab value="withdraw">Posts</v-tab>
+    </v-tabs>
+    <v-window v-model="tab">
+      <v-window-item value="deposit">
+        <h3>Entries</h3>
+      </v-window-item>
+
+      <v-window-item value="withdraw">
+        <h3>Posts</h3>
+      </v-window-item>
+    </v-window>
+  </v-card>
     </v-container>
   </template>
   
@@ -10,6 +25,7 @@
     data() {
       return {
         sniped: window.location.pathname.substring(1).toLowerCase(),
+        tab: null,
       };
     },
   };

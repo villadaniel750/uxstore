13118af2119f71c214d22dff9e8abac82b690aeb
villanuevaquelin <villanuevaquelin@gmail.com>
2024-09-25 15:03:35 -0300

    separated in components, fanpage tabs

---
+++ src/components/fan/CxEntries.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container fluid>
+      <v-row>
+        <v-col
+          v-for="i in 18"
+          :key="i"
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+          xl="2"
+        >
+          <v-img
+            :lazy-src="`https://picsum.photos/10/6?image=${i * 5 + 10}`"
+            :src="`https://picsum.photos/500/300?image=${i * 5 + 10}`"
+            height="200"
+            aspect-ratio="16/9"
+            cover
+          ></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
--- src/components/fan/CxTabBody.vue
+++ src/components/fan/CxTabBody.vue
@@ -10,37 +10,21 @@
       </v-tabs>
   
       <v-tabs-window v-model="tab">
-        <v-tabs-window-item
-          v-for="n in 3"
-          :key="n"
-          :value="n"
-        >
-          <v-container fluid>
-            <v-row>
-              <v-col
-                v-for="i in 18"
-                :key="i"
-                cols="12"
-                sm="6"
-                md="4"
-                lg="3"
-                xl="2"
-              >
-                <v-img
-                  :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
-                  :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
-                  height="200"
-                  aspect-ratio="16/9"
-                  cover
-                ></v-img>
-              </v-col>
-            </v-row>
-          </v-container>
+        <v-tabs-window-item :value="1">
+          <!-- Componente para Entries -->
+           <cx-entries></cx-entries>
+        </v-tabs-window-item>
+
+        <v-tabs-window-item :value="2">
+          <!-- Componente para Posts -->
+          <cx-posts></cx-posts>
         </v-tabs-window-item>
       </v-tabs-window>
     </v-card>
   </template>
   <script>
+  import CxEntries from "@/components/fan/CxEntries.vue";
+  import CxPosts from "@/components/fan/CxPosts.vue";
     export default {
       data: () => ({
         tab: null,

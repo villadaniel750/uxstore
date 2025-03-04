945ceaa9cb1610e40ad4f76461c2b8b1b12f0032
villadaniel750 <villanuevaquelin@gmail.com>
2025-03-04 01:09:11 -0300

    update client_id

--- src/components/login/CxXButton.vue
+++ src/components/login/CxXButton.vue
@@ -47,7 +47,7 @@
     methods: {
       async loginWithX() {
         localStorage.setItem("pre-login-url", window.location.pathname);
-        const clientId = "WHdUand1aHozTlZlNXEyMEt6NF86MTpjaQ";
+        const clientId = "a0dpSUhpRkxHckNLT0tkNTcteDk6MTpjaQ";
         const redirectUri = "https://earnlumens.org/auth/x/callback";
   
         // Genera y almacena el code_verifier

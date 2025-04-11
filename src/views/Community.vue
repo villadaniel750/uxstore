dd6bebd9053abf6dab207afa11e486283c06cf44
zerox-toml <passionkaizen96111@gmail.com>
2025-04-11 15:47:05 +0700

    feat:add title to Appbar menu

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -1,50 +1,42 @@
 <template>
-  <v-container fluid>
+<v-container fluid>
+    <v-list-item :active="activeItem === 'community'" v-slot:prepend="" class="my-4">
+      
+        <v-icon color="primary" size="24">mdi-handshake-outline</v-icon>
+        <v-list-item-title  class="ml-3 text-h6">
+          {{ $t('AppBar.community') }}
+        </v-list-item-title>
+    </v-list-item>
     <v-row>
-      <v-col
-        v-for="(img, index) in images"
-        :key="index"
-        cols="12"
-        sm="6"
-        md="4"
-        lg="3"
-        xl="2"
-      >
-        <v-img
-          :lazy-src="img.lazySrc"
-          :src="img.src"
-          alt="Image"
-          class="mb-2"
-          aspect-ratio="16/9"
-          rounded="lg"
-          @loadstart="loading[index] = true"
-          @load="loading[index] = false"
-        >
-          <template #placeholder>
-            <v-row class="fill-height ma-0" align="center" justify="center">
-              <v-progress-circular indeterminate color="primary" />
-            </v-row>
-          </template>
-        </v-img>
-      </v-col>
+        <v-col v-for="(img, index) in images" :key="index" cols="12" sm="6" md="4" lg="3" xl="2">
+            <v-img :lazy-src="img.lazySrc" :src="img.src" alt="Image" class="mb-2" aspect-ratio="16/9" rounded="lg" @loadstart="loading[index] = true" @load="loading[index] = false">
+                <template #placeholder>
+                    <v-row class="fill-height ma-0" align="center" justify="center">
+                        <v-progress-circular indeterminate color="primary" />
+                    </v-row>
+                </template>
+            </v-img>
+        </v-col>
     </v-row>
-  </v-container>
+</v-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      // Array de imágenes, que luego puedes reemplazar con la respuesta de tu webservice.
-      images: Array.from({ length: 18 }, (_, i) => {
-        const index = i + 1;
+    data() {
         return {
-          lazySrc: `https://picsum.photos/10/6?image=${index * 5 + 10}`,
-          src: `https://picsum.photos/500/300?image=${index * 5 + 10}`
+            // Array de imágenes, que luego puedes reemplazar con la respuesta de tu webservice.
+            images: Array.from({
+                length: 18
+            }, (_, i) => {
+                const index = i + 1;
+                return {
+                    lazySrc: `https://picsum.photos/10/6?image=${index * 5 + 10}`,
+                    src: `https://picsum.photos/500/300?image=${index * 5 + 10}`
+                };
+            }),
+            loading: Array(18).fill(false) // loading state for each image
         };
-      }),
-      loading: Array(18).fill(false) // loading state for each image
-    };
-  }
+    }
 };
 </script>

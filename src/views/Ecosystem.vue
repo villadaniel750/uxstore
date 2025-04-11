75fb2af8138d09fc99ef4639b33fcd80784f0f84
zerox-toml <passionkaizen96111@gmail.com>
2025-04-11 16:51:18 +0700

    feat:add pagenation to ecosystem.

--- src/views/Ecosystem.vue
+++ src/views/Ecosystem.vue
@@ -7,12 +7,22 @@
         </v-list-item-title>
     </v-list-item>
     <v-row>
-        <v-col v-for="(img, index) in images" :key="index" cols="12" sm="6" md="4" lg="3" xl="2">
+        <v-col v-for="(img, index) in paginatedImages" :key="index" cols="12" sm="6" md="4" lg="3" xl="2">
             <div>
                 <CxVideo :loading="loading[index]" :lazy-src="img.lazySrc" :src="img.src" :blocked="index % 3 === 1" :isPlaylist="index % 4 === 1" :playlist-count="index + 1" title="One meets his destiny on the road he takes to avoid it destiny on the road he takes to avoid it destiny on the road he takes to avoid it destiny on the road he takes to avoid it" username="Shamus" uploadDate="2024/10/7" :profileBadge="index % 3" />
             </div>
         </v-col>
     </v-row>
+
+    <!-- Pagination -->
+    <div class="d-flex justify-center mt-6">
+        <v-pagination
+            v-model="currentPage"
+            :length="totalPages"
+            :total-visible="7"
+            rounded="circle"
+        ></v-pagination>
+    </div>
 </v-container>
 </template>
 
@@ -25,8 +35,10 @@ export default {
     },
     data() {
         return {
+            currentPage: 1,
+            itemsPerPage: 48,
             images: Array.from({
-                length: 18
+                length: 150
             }, (_, i) => {
                 const index = i + 1;
                 return {
@@ -34,12 +46,53 @@ export default {
                     src: `https://picsum.photos/500/300?image=${index * 5 + 10}`,
                 };
             }),
-            loading: Array(18).fill(true),
+            loading: Array(150).fill(true),
         };
     },
+    computed: {
+        totalPages() {
+            return Math.ceil(this.images.length / this.itemsPerPage);
+        },
+        paginatedImages() {
+            const start = (this.currentPage - 1) * this.itemsPerPage;
+            const end = start + this.itemsPerPage;
+            return this.images.slice(start, end);
+        }
+    },
+    watch: {
+        currentPage() {
+            // Scroll to top
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth' // This makes it smooth scroll
+            });
+
+            // Existing loading state code
+            const start = (this.currentPage - 1) * this.itemsPerPage;
+            const end = start + this.itemsPerPage;
+            this.loading = Array(this.images.length).fill(true);
+            
+            setTimeout(() => {
+                const newLoading = [...this.loading];
+                for (let i = start; i < end; i++) {
+                    if (i < this.loading.length) {
+                        newLoading[i] = false;
+                    }
+                }
+                this.loading = newLoading;
+            }, 1000);
+        }
+    },
     mounted() {
+        // Initial loading state
         setTimeout(() => {
-            this.loading = this.loading.map(() => false);
+            const newLoading = [...this.loading];
+            for (let i = 0; i < this.itemsPerPage; i++) {
+                if (i < this.loading.length) {
+                    newLoading[i] = false;
+                }
+            }
+            this.loading = newLoading;
         }, 1000);
     },
 };

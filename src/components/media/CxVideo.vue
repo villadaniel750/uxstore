beb0cf946602efa46383d54823f99f1aa436b6b5
barneylogo <ben.amwithyou@gmail.com>
2025-04-23 11:41:35 +0900

    FF-07: modal flow init

--- src/components/media/CxVideo.vue
+++ src/components/media/CxVideo.vue
@@ -9,6 +9,7 @@
       @loadstart="$emit('loadstart')"
       @load="loadFinished"
       class="cursor-pointer"
+      @click="handleModalOpen"
     >
       <template #placeholder>
         <v-row class="fill-height ma-0" align="center" justify="center">
@@ -30,28 +31,47 @@
       </v-btn>
     </v-overlay>
 
-    <div 
-        v-if="isPlaylist"
-        class="position-absolute right-0 bottom-0 text-body-2 bg-black opacity-60 rounded pb-1 pt-1 pl-1 pr-1 mr-3 mb-3 elevation-2"
-        style="z-index: 9999999;"
+    <div
+      v-if="isPlaylist"
+      class="position-absolute right-0 bottom-0 text-body-2 bg-black opacity-60 rounded pb-1 pt-1 pl-1 pr-1 mr-3 mb-3 elevation-2"
+      style="z-index: 9999999"
     >
-        <b class="opacity-100">13:02</b>
+      <b class="opacity-100">13:02</b>
     </div>
 
-    <div 
-        v-if="!isPlaylist && playlistCount%3===0"
-        class="d-flex align-center position-absolute right-0 bottom-0 text-body-2 opacity-60 bg-black rounded pb-1 pt-1 pl-1 pr-1 mr-3 mb-3 elevation-3"
-        style="z-index: 9999999;"
+    <div
+      v-if="!isPlaylist && playlistCount % 3 === 0"
+      class="d-flex align-center position-absolute right-0 bottom-0 text-body-2 opacity-60 bg-black rounded pb-1 pt-1 pl-1 pr-1 mr-3 mb-3 elevation-3"
+      style="z-index: 9999999"
     >
-        <v-icon icon="mdi-playlist-play" class="opacity-100" size="small"></v-icon>
-        <b class="opacity-100">{{ playlistCount }}</b>
+      <v-icon
+        icon="mdi-playlist-play"
+        class="opacity-100"
+        size="small"
+      ></v-icon>
+      <b class="opacity-100">{{ playlistCount }}</b>
     </div>
   </v-card>
   <div v-if="imageLoaded" class="d-flex justify-space-between">
-
     <div class="d-flex align-center">
-      <v-card :class="['ma-4', selectedClass]" height="45" width="45" min-width="45" min-height="45" flat tile color="transparent">
-        <v-img lazy-src="https://randomuser.me/api/portraits/men/22.jpg" src="https://randomuser.me/api/portraits/men/22.jpg" alt="Image" cover class="fill-height" rounded="circle" />
+      <v-card
+        :class="['ma-4', selectedClass]"
+        height="45"
+        width="45"
+        min-width="45"
+        min-height="45"
+        flat
+        tile
+        color="transparent"
+      >
+        <v-img
+          lazy-src="https://randomuser.me/api/portraits/men/22.jpg"
+          src="https://randomuser.me/api/portraits/men/22.jpg"
+          alt="Image"
+          cover
+          class="fill-height"
+          rounded="circle"
+        />
       </v-card>
 
       <div class="">
@@ -59,33 +79,38 @@
           {{ title }}
         </div>
         <div class="w-100 text-no-wrap overflow-hidden text-left">
-          <span class="text-caption font-weight-bold ">{{username}}</span>
+          <span class="text-caption font-weight-bold">{{ username }}</span>
           <v-img
-              v-if="profileBadge === 1"
-              class="d-inline-block ml-1"
-              style="vertical-align: middle;"
-              height="14"
-              width="14"
-              src="/src/assets/blue-verified-badge.svg"
-            ></v-img>
+            v-if="profileBadge === 1"
+            class="d-inline-block ml-1"
+            style="vertical-align: middle"
+            height="14"
+            width="14"
+            src="/src/assets/blue-verified-badge.svg"
+          ></v-img>
           <v-img
             v-if="profileBadge === 2"
             class="d-inline-block ml-1"
-            style="vertical-align: middle;"
+            style="vertical-align: middle"
             height="14"
             width="14"
             src="/src/assets/yellow-verified-badge.svg"
           ></v-img>
-          <span class="text-caption ml-1">{{" • " + uploadDate}}</span>
+          <span class="text-caption ml-1">{{ " • " + uploadDate }}</span>
         </div>
       </div>
-
     </div>
 
     <div class="pt-4">
       <v-menu>
         <template v-slot:activator="{ props: activatorProps }">
-          <v-btn variant="text" prepend-icon="mdi-dots-vertical" width="10" size="x-small" v-bind="activatorProps"></v-btn>
+          <v-btn
+            variant="text"
+            prepend-icon="mdi-dots-vertical"
+            width="10"
+            size="x-small"
+            v-bind="activatorProps"
+          ></v-btn>
         </template>
 
         <v-list>
@@ -104,14 +129,46 @@
           <v-list-item @click="onClick">
             <v-list-item-title>Contact</v-list-item-title>
           </v-list-item>
-
         </v-list>
       </v-menu>
     </div>
   </div>
+
+  <!-- Login Modal -->
+  <v-dialog v-model="isLoginModalVisible" max-width="500">
+    <v-card>
+      <v-card-title class="headline">Login Required</v-card-title>
+      <v-card-text>
+        <!-- Login form or message -->
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" text @click="isLoginModalVisible = false"
+          >Close</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <!-- Wallet Connect Modal -->
+  <v-dialog v-model="isWalletModalVisible" max-width="500">
+    <v-card>
+      <v-card-title class="headline">Connect Wallet</v-card-title>
+      <v-card-text>
+        <!-- Wallet connection form or message -->
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" text @click="isWalletModalVisible = false"
+          >Close</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   props: {
     lazySrc: {
@@ -127,40 +184,53 @@ export default {
       default: false,
     },
     isPlaylist: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     playlistCount: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     title: {
       type: String,
-      default: ""
+      default: "",
     },
     username: {
       type: String,
-      default: ""
+      default: "",
     },
     uploadDate: {
       type: String,
-      default: ""
+      default: "",
     },
     profileBadge: {
       type: Number,
-      default: 0
-    }
+      default: 0,
+    },
   },
   data() {
     return {
-      imageLoaded: false, // Define variable
+      imageLoaded: false,
+      isLoginModalVisible: false,
+      isWalletModalVisible: false,
     };
   },
   methods: {
     loadFinished() {
       this.imageLoaded = true;
-    }
-  }
+    },
+    handleModalOpen() {
+      if (!this.loggedIn) {
+        this.isLoginModalVisible = true;
+      } else if (!this.walletConnected) {
+        this.isWalletModalVisible = true;
+      } else {
+      }
+    },
+  },
+  computed: {
+    ...mapGetters(["loggedIn", "walletConnected"]),
+  },
 };
 </script>
 

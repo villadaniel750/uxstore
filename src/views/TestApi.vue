be1192c2a28a74f1be29be0a55673c8bf0a5510d
villadaniel750 <villanuevaquelin@gmail.com>
2025-05-01 01:10:03 -0300

    webworker implementation for jwt secure store

--- src/views/TestApi.vue
+++ src/views/TestApi.vue
@@ -4,7 +4,10 @@
       <v-card-title>Test API</v-card-title>
       <v-card-text>
         <v-btn @click="fetchData" color="primary">Fetch API</v-btn>
+        <v-btn @click="readFromWorker" color="secondary" class="ml-2">Read from WebWorker</v-btn>
+
         <p v-if="response">Response: {{ response }}</p>
+        <p v-if="workerToken">Worker Token: {{ workerToken }}</p>
         <p v-if="error" class="text-red">{{ error }}</p>
       </v-card-text>
     </v-card>
@@ -14,10 +17,12 @@
 <script>
 import { ref } from 'vue';
 import api from "@/api";
+import jwtWorker from '@/webworker/jwtWorkerClient.js';
 
 export default {
   setup() {
     const response = ref(null);
+    const workerToken = ref(null);
     const error = ref(null);
 
     const fetchData = async () => {
@@ -27,12 +32,23 @@ export default {
       try {
         const accessToken = await api.refreshAccessToken();
         response.value = accessToken;
+
+        // Guarda el token en el Web Worker
+        jwtWorker.setToken(accessToken);
       } catch (err) {
         error.value = `Error fetching API: ${err.message}`;
       }
     };
 
-    return { response, error, fetchData };
+    const readFromWorker = async () => {
+      try {
+        workerToken.value = await jwtWorker.getToken();
+      } catch (err) {
+        error.value = `Error reading from worker: ${err.message}`;
+      }
+    };
+
+    return { response, workerToken, error, fetchData, readFromWorker };
   }
 };
 </script>
